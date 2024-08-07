2a8baf443c92ef38358a594d0f688bf68ecff26f
kenway214 <kenway214@outlook.com>
2024-08-07 14:21:40 +0530

    sm8450-common : Enable double tap to wake feature
    Revert "sm8450-common: Move to double tap sensor"
    
    This reverts commit fba1a4d5fea82c9e95b61426fabe59fcf430c4f5.
    
    Revert "sm8450-common: Enable udfps and single_tap sensors"
    
    This reverts commit b9e3d602aba103c5b668b1d3d5dd85f06c4bf260.
    
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -204,6 +204,9 @@ endif
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := taro
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+
 # PowerShare
 $(call soong_config_set, XIAOMI_POWERSHARE, WIRELESS_TX_ENABLE_PATH, /sys/class/qcom-battery/reverse_chg_mode)
 
